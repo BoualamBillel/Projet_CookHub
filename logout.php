243146c0810a20c0ea089a35a7c486b0d2731351
Jonathan S <jonset31@hotmail.com>
2025-08-04 16:56:10 +0200

    crud update

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Démarre la session pour gérer les connexions des utilisateurs
-session_unset(); // Supprime toutes les variables de session
-session_destroy(); // Détruit la session
-header('Location: login.php'); // Redirection vers la page de connexion
-exit(); // Termine le script
-
-?>
