9704dcb287b1185ec4ef975a53b137cfa429cbc5
Billel Boualam <boualambillelsofien@gmail.com>
2025-08-04 15:03:39 +0200

    CRUD User rempli

---
+++ bdd.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS CookHub_users 
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CookHub_recettes 
+(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES CookHub_users(id)
+);
