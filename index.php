09d9be9d2c3693220892ebb8aebbd9fdd75779c4
Arthur LEMAIRE <arthur.lemaire86@gmail.com>
2025-08-04 15:06:15 +0200

    Squelette index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1></h1>
+    </header>
+    <main>
+        <div class="list_recipe">
+            <?php foreach($recettes as $recette):?>
+                <div class="card_recipe">
+                    <div class="recipe_ingredrients">
+                        <p>
+                            <?= $recette['ingredients']?>
+                        </p>
+                    </div>
+                    <div class="recipe_infos">
+                        <h2>
+                            <?= $recette['title']?>
+                        </h2>
+                        <p>
+                            <?= $recette['cooking_time']?>
+                        </p>
+                        <p>
+                            ID : <?= $recette['user_id']?>
+                        </p>
+                    </div>
+                </div>
+            <?php endforeach?>
+        </div>
+    </main>
+    <footer>
+        <div class="logout"></div>
+    </footer>
+</body>
+</html>
